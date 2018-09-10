70c9488bddd4dee71c0a6159d85b1049b8cecb93
Mark Wolff <Mark.Wolff@microsoft.com>
2018-09-10 14:33:35 -0700

    Export IDiagnosticLogger in module, change from default to named export (#17)

--- applicationinsights-core-js.ts
+++ applicationinsights-core-js.ts
@@ -4,9 +4,10 @@ export { ITelemetryPlugin, IPlugin } from "./JavaScriptSDK.Interfaces/ITelemetry
 export { IAppInsightsCore } from "./JavaScriptSDK.Interfaces/IAppInsightsCore";
 export { ITelemetryItem } from "./JavaScriptSDK.Interfaces/ITelemetryItem";
 export { INotificationListener } from "./JavaScriptSDK.Interfaces/INotificationListener";
+export { IDiagnosticLogger } from "./JavaScriptSDK.Interfaces/IDiagnosticLogger";
 export { EventsDiscardedReason } from "./JavaScriptSDK.Enums/EventsDiscardedReason";
 export { AppInsightsCore } from "./JavaScriptSDK/AppInsightsCore";
 export { CoreUtils } from "./JavaScriptSDK/CoreUtils";
 export { NotificationManager } from "./JavaScriptSDK/NotificationManager";
-export {DiagnosticLogger, _InternalLogMessage} from './JavaScriptSDK/DiagnosticLogger';
-export {_InternalMessageId, LoggingSeverity} from './JavaScriptSDK.Enums/LoggingEnums';
+export { DiagnosticLogger, _InternalLogMessage } from './JavaScriptSDK/DiagnosticLogger';
+export { _InternalMessageId, LoggingSeverity } from './JavaScriptSDK.Enums/LoggingEnums';
