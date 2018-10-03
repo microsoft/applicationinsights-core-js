608892b8231bf7655330d6db90c083d0cf3344b7
Mark Wolff <Mark.Wolff@microsoft.com>
2018-10-02 17:14:36 -0700

    add tslint (#25)
    * add tslint

--- JavaScriptSDK/CoreUtils.ts
+++ JavaScriptSDK/CoreUtils.ts
@@ -13,6 +13,7 @@ export class CoreUtils {
 
     public static newGuid():  string  {
         return  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(GuidRegex,  function  (c) {
+            // tslint:disable-next-line:insecure-random
             var  r  =  (Math.random()  *  16  |  0),  v  =  (c  ===  'x'  ?  r  :  r  &  0x3  |  0x8);
             return  v.toString(16);
         });
