3c68e98b85b6db96a3440b90038039c64454e475
Mark Wolff <marwolff@microsoft.com>
2018-10-26 13:47:30 -0700

    Add node entry point

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import {uglify} from "rollup-plugin-uglify";
 import replace from "rollup-plugin-replace";
 
 const version = require("./package.json").version;
+const outputName = "applicationinsights-core-js";
 const banner = [
   "/*!",
   ` * Application Insights JavaScript SDK - Core, ${version}`,
@@ -13,9 +14,9 @@ const banner = [
 
 const browserRollupConfigFactory = isProduction => {
   const browserRollupConfig = {
-    input: "dist-esm/applicationinsights-core-js.js",
+    input: `dist-esm/${outputName}.js`,
     output: {
-      file: "browser/applicationinsights-core-js.js",
+      file: `browser/${outputName}.js`,
       banner: banner,
       format: "umd",
       name: "aicore",
@@ -38,7 +39,7 @@ const browserRollupConfigFactory = isProduction => {
   };
 
   if (isProduction) {
-    browserRollupConfig.output.file = "browser/applicationinsights-core-js.min.js";
+    browserRollupConfig.output.file = `browser/${outputName}.min.js`;
     browserRollupConfig.plugins.push(
       uglify({
         output: {
@@ -55,7 +56,44 @@ const browserRollupConfigFactory = isProduction => {
   return browserRollupConfig;
 };
 
+const nodeUmdRollupConfigFactory = (isProduction) => {
+  const nodeRollupConfig = {
+    input: `dist-esm/${outputName}.js`,
+    output: {
+      file: `dist/${outputName}.js`,
+      banner: banner,
+      format: "umd",
+      name: "aicore",
+      sourcemap: true
+    },
+    plugins: [
+      replace({
+        delimiters: ["", ""],
+        values: {
+          "// Copyright (c) Microsoft Corporation. All rights reserved.": "",
+          "// Licensed under the MIT License.": ""
+        }
+      }),
+      nodeResolve()
+    ]
+  };
+
+  if (isProduction) {
+    nodeRollupConfig.output.file = `dist/${outputName}.min.js`;
+    nodeRollupConfig.plugins.push(
+      uglify({
+        output: {
+          preamble: banner
+        }
+      })
+    );
+  }
+
+  return nodeRollupConfig;
+}
+
 export default [
+  nodeUmdRollupConfigFactory(true),
   browserRollupConfigFactory(true),
   browserRollupConfigFactory(false)
 ];
